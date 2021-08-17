679e30de1185a7cbac7c4c53190eba85b8bedfb7
Brittany Harrison <bkharrison94@gmail.com>
2021-08-17 13:29:55 +0100

    db instance added

--- main.tf
+++ main.tf
@@ -32,6 +32,16 @@ resource "aws_subnet" "eng89_brittany_subnet_public_terra" {
   }
 }
 
+resource "aws_subnet" "eng89_brittany_subnet_private_terra" {
+  vpc_id     = aws_vpc.eng89_brittany_vpc_terra.id
+  cidr_block = "10.104.2.0/24"
+  map_public_ip_on_launch = false
+  availability_zone = "eu-west-1a"
+
+  tags = {
+    Name = "eng89_brittany_subnet_private_terra"
+  }
+}
 
 resource "aws_route_table" "eng89_brittany_rt_terra" {
   vpc_id = aws_vpc.eng89_brittany_vpc_terra.id
@@ -152,6 +162,46 @@ resource "aws_network_acl" "eng89_brittany_public_nacl_terra" {
 }
 
 
+resource "aws_network_acl" "eng89_brittany_nacl_private_terra" {
+	vpc_id = aws_vpc.eng89_brittany_vpc_terra.id
+	subnet_ids = [aws_subnet.eng89_brittany_subnet_private_terra.id]
+
+	# allow SSH from public subnet
+	ingress {
+		protocol = "tcp"
+		rule_no = 100
+		action = "allow"
+		cidr_block = "10.104.1.0/24"
+		from_port = 22
+		to_port = 22
+	}
+
+	# allow 27017 from public subnet
+	ingress {
+		protocol = "tcp"
+		rule_no = 110
+		action = "allow"
+		cidr_block = "10.104.1.0/24"
+		from_port = 27017
+		to_port = 27017
+	}
+	
+	# allow ephemeral to public subnet
+	egress {
+		protocol = "tcp"
+		rule_no = 100
+		action = "allow"
+		cidr_block = "10.104.1.0/24"
+		from_port = 1024
+		to_port = 65535
+	}
+
+	tags = {
+		Name = "eng89_brittany_nacl_private_terra"
+	}
+}
+
+
 resource "aws_security_group" "eng89_brittany_sg_app_terra"{
 	name = "eng89_brittany_sg_app_terra"
 	description = "Allow public access for nodejs instnace"
@@ -189,10 +239,47 @@ resource "aws_security_group" "eng89_brittany_sg_app_terra"{
         cidr_blocks = ["0.0.0.0/0"]
     }
 
+    egress {
+		description = "All traffic out"
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+	}
+
+
 	tags = {
 		Name = "eng89_brittany_sg_app_terra"
 	}
 }
+
+resource "aws_security_group" "db_sg"{
+	name = "eng89_brittany_sg_db_terra"
+	description = "Allow traffic on port 27017 for mongoDB"
+	vpc_id = var.vpc_id
+
+	ingress {
+		description = "27017 from app instance"
+		from_port = 27017
+		to_port = 27017
+		protocol = "tcp"
+		security_groups = [var.app_sg]
+	}
+
+	egress {
+		description = "All traffic out"
+		from_port = 0
+		to_port = 0
+		protocol = "-1"
+		cidr_blocks = ["0.0.0.0/0"]
+
+	}
+
+	tags = {
+		Name = "eng89_brittany_sg_db_terra"
+	}
+}
+
 
 
 resource "aws_instance" "app_instance"{
@@ -205,8 +292,36 @@ resource "aws_instance" "app_instance"{
     tags = {
         Name = "eng89_brittany_app_terra"
     } 
-
    
+#    connection {
+# 		type = "ssh"
+# 		user = "ubuntu"
+# 		private_key = "${file(var.aws_key_path)}"
+# 		host = "${self.public_ip}"
+# 	} 
+
+# 	# export private ip of mongodb instance and start app
+# 	provisioner "remote-exec"{
+# 		inline = [
+# 			"echo \"export DB_HOST=${var.db_private_ip}\" >> /home/ubuntu/.bashrc",
+# 			"export DB_HOST=${var.db_private_ip}",
+# 			"cd app/ && pm2 start app.js",
+# 		]
+# 	}
+
+}
+
+
+resource "aws_instance" "db_instance" {
+	ami = var.db_ami
+	subnet_id = var.private_subnet_id
+	instance_type = var.instance_type
+	key_name = var.aws_key_name
+	associate_public_ip_address = false
+	vpc_security_group_ids = [var.db_sg]
+	tags = {
+		Name = "eng89_brittany_db_terra"
+	}
 }
 
 
--- variable.tf
+++ variable.tf
@@ -18,6 +18,10 @@ variable "app_ami" {
     default = "ami-0f18a434af2396095"
 }
 
+variable "db_ami"{
+    default = "ami-03b79bde6be632efd"
+}
+
 variable "my_ip" {
     default = "81.134.180.37/32"
 
@@ -32,7 +36,19 @@ variable "subnet_id" {
     default = "subnet-0cb055a0863f12127"
   
 }
-
+variable "private_subnet_id" {
+    default = "subnet-0143b0240ee1a7ca2" 
+  
+}
 variable "app_sg" {
     default = "sg-0f9ee8580b0ff6179"
+}
+
+variable "db_sg"{
+    default = "sg-0698bb1ccfd7a46fc"
+}
+
+variable "db_private_ip" {
+    default = "10.104.2.0"
+  
 }
